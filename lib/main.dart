175bf21db54b4edbb1cdb74b16afe16393f7c4a1
ayogatot <azerino25@gmail.com>
2019-10-11 14:21:51 +0700

    Cointainer

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Belajar Container'),
+          title: Text(
+            'Belajar Container',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
+        body: Container(
+          color: Colors.teal,
+          padding: EdgeInsets.all(20),
+          child: Container(
+            margin: EdgeInsets.all(30),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.amber, Colors.white70])),
+          ),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          child: Container(
+            height: 50.0,
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => print('Pressed'),
+          tooltip: 'Increment Counter',
+          child: Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
   }
